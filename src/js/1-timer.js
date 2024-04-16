9cc9c17c9c7735adec47e781ae4b4a1ada4a4781
olhaHrb <150930419+olhaHrb@users.noreply.github.com>
2024-04-16 18:06:15 +0300

    marking and styles snackbar

---
+++ src/js/1-timer.js
@@ -0,0 +1,20 @@
+// Описаний в документації
+import flatpickr from "flatpickr";
+// Додатковий імпорт стилів
+import "flatpickr/dist/flatpickr.min.css";
+
+let userSelectedDate = 0;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+const selector = document.querySelector("#datetime-picker");
+
+flatpickr(selector, options);

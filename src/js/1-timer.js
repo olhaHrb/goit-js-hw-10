e09709392cd615af50fd2b5f2263f5a32de683ad
olhaHrb <150930419+olhaHrb@users.noreply.github.com>
2024-04-17 11:32:36 +0300

    timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,20 +1,27 @@
-// Описаний в документації
 import flatpickr from "flatpickr";
-// Додатковий імпорт стилів
 import "flatpickr/dist/flatpickr.min.css";
 
 let userSelectedDate = 0;
 
+const now = new Date();
+console.log (now);
+
+const date = document.querySelector("#datetime-picker");
+date.addEventListener("click", event => {
+  
+});
+
+
 const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        console.log(selectedDates[0]);
+    },
 };
 
-const selector = document.querySelector("#datetime-picker");
 
-flatpickr(selector, options);
+
+//flatpickr(selector, options);

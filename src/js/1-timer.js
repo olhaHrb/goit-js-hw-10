9c895e545443c112a7c5604e64f69108a74c9ddc
olhaHrb <150930419+olhaHrb@users.noreply.github.com>
2024-04-17 13:30:06 +0300

    date validation

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -3,25 +3,39 @@ import "flatpickr/dist/flatpickr.min.css";
 
 let userSelectedDate = 0;
 
-const now = new Date();
-console.log (now);
-
+const now = Date.now();
+const button = document.querySelector('button[data-start]');
+const btnIsActive = false;
+button.addEventListener("click", createTimer);
 const date = document.querySelector("#datetime-picker");
-date.addEventListener("click", event => {
+date.addEventListener("click", hadleClick);
+function hadleClick() {
+  flatpickr();
+};
+
+function createTimer() {
   
-});
+};
 
-
+const selector = date;
 const options = {
     enableTime: true,
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-        console.log(selectedDates[0]);
+      console.log(selectedDates[0]);
+      console.log(selectedDates[0].getTime());
+      if (selectedDates[0].getTime() < now) {
+        window.alert("Please choose a date in the future");
+      } else {        
+        userSelectedDate = selectedDates[0];
+        return userSelectedDate;
+      };      
     },
 };
 
+flatpickr(selector, options);
 
+console.log(userSelectedDate);
 
-//flatpickr(selector, options);
